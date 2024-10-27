03a7c095ac3b742a980398a387fb89096ffaa4b9
zihad868 <zihadh868@gmail.com>
2024-10-27 11:12:06 +0600

    Setup Tailwind & React Router

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>Food Cart</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
