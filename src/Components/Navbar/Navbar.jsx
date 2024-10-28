ca4998ffdff4d6a56c004254ee3be2b307e41535
zihad868 <zihadh868@gmail.com>
2024-10-28 12:01:10 +0600

    Cart Drawer

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,34 +1,68 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
 import { FaCartShopping } from "react-icons/fa6";
 import logo from "../../assets/chilli.png";
 
 const Navbar = () => {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  const toggleDrawer = () => {
+    setIsDrawerOpen(true);
+  };
+
+  const toggleDrawerClose = () => {
+    setIsDrawerOpen(false);
+  };
+
   return (
-    <div className="flex justify-evenly lg:mr-40 lg:ml-10 mt-5">
-      <div>
-        <ul className="space-x-3 uppercase flex mt-2">
-          <div>
-            <a href="/">
-              <img className="w-5 h-5" src={logo} alt="" />
-            </a>
+    <div>
+      <div className="flex justify-evenly lg:mr-40 lg:ml-10 mt-6">
+        <div>
+          <ul className="space-x-3 uppercase flex mt-2">
+            <Link to="#">
+              <img className="w-5 h-5" src={logo} alt="Logo" />
+            </Link>
+            <NavLink to="#">Menu</NavLink>
+            <NavLink to="#">Rewards</NavLink>
+            <NavLink to="#">Locations</NavLink>
+            <NavLink to="#">Gift Cards</NavLink>
+            <NavLink to="#">Log In</NavLink>
+          </ul>
+        </div>
+
+        <div className="flex space-x-3">
+          <div
+            className="mt-2 text-3xl mr-3 cursor-pointer relative"
+            onClick={toggleDrawer}
+          >
+            <FaCartShopping />
+            <p className="bg-red-500 text-white font-bold rounded-lg absolute -top-7 ml-2 px-1">
+              0
+            </p>
           </div>
-          <NavLink>Menu</NavLink>
-          <NavLink>Rewords</NavLink>
-          <NavLink>Locations</NavLink>
-          <NavLink>Gift Cards</NavLink>
-          <NavLink>Log In</NavLink>
-        </ul>
+          <button className="uppercase font-bold bg-red-500 px-2 py-1 rounded-md text-white">
+            order now
+          </button>
+        </div>
       </div>
 
-      <div className="flex space-x-3">
-        <div className="mt-2 text-3xl mr-3">
-          <FaCartShopping />
+      {/* Drawer */}
+      {isDrawerOpen && (
+        <div className="fixed top-0 right-0 h-full w-64 bg-red-500 shadow-lg z-50 transition-transform transform translate-x-0 mt-4 mr-3">
+          <div className="bg-white w-full flex justify-between">
+            <div className="mt-4 text-red-500 text-xl flex">
+              <FaCartShopping />
+              <p className="ml-3"><span>0</span> item</p>
+            </div>
+            <button
+              onClick={toggleDrawerClose}
+              className="text-red-500 border border-red-600 p-2 rounded-md mb-5"
+            >
+              Close
+            </button>
+          </div>
         </div>
-        <button className="uppercase font-bold bg-red-500 px-2 py-1 rounded-md text-white">
-          order now
-        </button>
-      </div>
+      )}
     </div>
   );
 };
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from '../../Components/Navbar/Navbar';
 
+
 const Home = () => {
     return (
         <div>
